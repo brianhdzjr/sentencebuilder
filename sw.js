ed45367984941346adc9b40b90ec852730bf1ed7
brianhdzjr <contact@brianhdz.com>
2026-05-31 21:59:10 -0400

    Update sw.js
    UPDATED SW,JS

--- sw.js
+++ sw.js
@@ -1,24 +1,38 @@
-const CACHE_NAME = 'aac-grid-v1';
+const CACHE_NAME = 'gridtalk-v2';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  // Cache the database library for offline use
+  'https://unpkg.com/dexie/dist/dexie.js'
 ];
 
-// Install event: Cache the files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
 
-// Fetch event: Serve from cache if offline
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
   );
 });
